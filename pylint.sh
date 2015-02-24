f64d5ac1e2ff13d1d7e012d2c63b14da4f75f2b5
Pavel Fux <pavel@stratoscale.com>
2015-02-24 15:29:58 +0200

    fixing logging related errors:
    Module 'logging' has no 'progress' member
    Module 'logging' has no 'success' member

--- pylint.sh
+++ pylint.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 DIR_PATH=`dirname $0`
-find $1 -type f -iname "*.py" -exec pylint --rcfile=$DIR_PATH/.pylintrc -r n '{}' ';' | ack --passthru --nocolor --match ".*"
+PYTHONPATH=$DIR_PATH:$PYTHONPATH find $1 -type f -iname "*.py" -exec pylint --rcfile=$DIR_PATH/.pylintrc -r n '{}' ';' | ack --passthru --nocolor --match ".*"
 if [ $? == 0 ] ; then
   exit 1
 fi
