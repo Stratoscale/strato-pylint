d85b60d49be4a986221571394ef5d63ee8f644fe
Shoham Peller <shoham.peller@stratoscale.com>
2015-09-08 17:50:18 +0300

    Added the possibility to export CHECK_CONVENTIONS=yes, and pylint will check your naming conevtions too

--- pylint.sh
+++ pylint.sh
@@ -5,7 +5,12 @@ if [ -z "${PYLINTRC_PATH}" ]; then
     PYLINTRC_PATH=$DIR_PATH/.pylintrc
 fi
 
-PYTHONPATH=$DIR_PATH:$PYTHONPATH find $1 -type f -iname "*.py" -exec pylint --rcfile=$PYLINTRC_PATH -r n '{}' ';' | ack --passthru --nocolor --match ".*"
+ENABLE_CHECKER=""
+if [ $CHECK_CONVENTIONS = yes ] ; then
+	ENABLE_CHECKER="C0102,C0103,C0112,C0121,C0202,C0203,C0204,C0303,C0304,C0321,C0322,C0323,C0324,C1001"
+fi
+
+PYTHONPATH=$DIR_PATH:$PYTHONPATH find $1 -type f -iname "*.py" -exec pylint --enable="$ENABLE_CHECKER" --rcfile=$PYLINTRC_PATH -r n '{}' ';' | ack --passthru --nocolor --match ".*"
 if [ $? == 0 ] ; then
   exit 1
 fi
